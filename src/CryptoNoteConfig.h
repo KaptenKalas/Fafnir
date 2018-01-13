bc48db10e7d8f54396e50e95bc2a6747d12bb070
BengtG <bengtgoran_4u@hotmail.com>
2018-01-13 14:34:32 +0100

    add in gensis hash

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -63,7 +63,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "Fafnir";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00018d93f5d771029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121010387448c867563e461480bea8889de46c100d221d810bfca6bd1333399a87a96";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
