5cf528fabe220ad91a09eb74811145ae22a4e4ac
Svenne Banan <s@s.se>
2018-01-13 13:55:54 +0100

    more settings

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -19,7 +19,7 @@ const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(8000000000000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(8000000000000000); //80M coins * 10^8 atomic parts
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
